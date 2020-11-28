bd236952e3254229d9fe15cc039d107e41451065
Ander Benito <anderbenito96@gmail.com>
2020-11-28 18:18:18 +0100

    Changed http cookie domain

--- src/utils/sendRefreshToken.ts
+++ src/utils/sendRefreshToken.ts
@@ -1,5 +1,11 @@
 import { Response } from "express";
 
 export const sendRefreshToken = (res: Response, token: string) => {
-	return res.cookie("jid", token, { httpOnly: true });
+	return res.cookie("jid", token, {
+		httpOnly: true,
+		domain:
+			process.env.NODE_ENV === "production"
+				? "https://algoserio.herokuapp.com/"
+				: "http://localhost:5000",
+	});
 };
